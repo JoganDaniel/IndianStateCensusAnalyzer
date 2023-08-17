14c85f34cdf0b564cef1318f85581685a516e4f5
Jogan Daniel A <jogan.d@tvsnext.com>
2023-08-17 12:49:39 +0530

    [jogan] Indian state census program exception handling

--- IndianStateCensusAnalyzer/StateCensusAnalyzer.cs
+++ IndianStateCensusAnalyzer/StateCensusAnalyzer.cs
@@ -1,4 +1,5 @@
 ﻿using CsvHelper;
+using IndianStateCensusAnalyser;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -12,6 +13,20 @@ namespace IndianStateCensusAnalyzer
     {
         public static int ReadStateCensusData(string filepath)
         {
+            if (!Path.GetExtension(filepath).Equals(".csv"))
+            {
+                throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.FILE_INCORRECT, "File extension incorrect");
+            }
+
+            else if (!File.Exists(filepath))
+            {
+                throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.FILE_NOT_EXISTS, "File does not exist");
+            }
+
+            else if (!File.ReadAllLines(filepath)[0].Equals("State,Population,AreaInSqKm,DensityPerSqKm"))
+            {
+                throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.HEADER_INCORRECT, "Header Incorrect");
+            }
             using (var reader = new StreamReader(filepath))
             {
                 using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
--- IndianStateCensusAnalyzerTest/UnitTest1.cs
+++ IndianStateCensusAnalyzerTest/UnitTest1.cs
@@ -1,3 +1,4 @@
+using IndianStateCensusAnalyser;
 using IndianStateCensusAnalyzer;
 
 namespace IndianStateCensusAnalyzerTest
@@ -6,11 +7,51 @@ namespace IndianStateCensusAnalyzerTest
     {
 
         public string stateCensusDataFilePath = @"E:\Bridgelabz\IndianStateCensusAnalyzer\IndianStateCensusAnalyzer\IndianStateCensusAnalyzer\Files\StateCensusData.csv";
+        public string stateCensusDataFilePathincorrect = @"E:\Bridgelabz\IndianStateCensusAnalyzer\IndianStateCensusAnalyzer\IndianStateCensusAnalyzer\Files\StateCensusData.txt";
+        public string stateCensusDataFilePathNotExist = @"jhuf.csv";
         [Test]
         public void GivenStateCensusData_WhenAnalysed_RecordsShouldBeMatched()
         {
             Assert.AreEqual(StateCensusAnalyzer.ReadStateCensusData(stateCensusDataFilePath),
                 CSVStateCensus.ReadStateCensusData(stateCensusDataFilePath));
+        }
+        [Test]
+        public void GivenStateCencusDataFileIncorrect_WhenAnalysed_ShouldReturnException()
+        {
+            try
+            {
+                StateCensusAnalyzer.ReadStateCensusData(stateCensusDataFilePathincorrect);
+            }
+            catch (CensusAnalyserException ex)
+            {
+                Assert.AreEqual(ex.Message, "File extension incorrect");
+            }
+
+        }
+        [Test]
+        public void GivenStateCencusDataFileNotExists_WhenAnalysed_ShouldReturnException()
+        {
+            try
+            {
+                StateCensusAnalyzer.ReadStateCensusData(stateCensusDataFilePathNotExist);
+            }
+            catch (CensusAnalyserException ex)
+            {
+                Assert.AreEqual(ex.Message, "File does not exist");
+            }
+
+        }
+        [Test]
+        public void GivenStateCensusHeaderIncorrect_WhenAnalysed_ShouldReturnException()
+        {
+            try
+            {
+                StateCensusAnalyzer.ReadStateCensusData(stateCensusDataFilePath);
+            }
+            catch (CensusAnalyserException ex)
+            {
+                Assert.AreEqual(ex.Message, "Header incorrect");
+            }
         }
     }
 }
